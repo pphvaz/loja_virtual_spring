94602213e5da7aed6d336e326015db9df7c85759
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-21 17:07:06 -0300

    Commit inicial - Projeto Loja Virtual Criado

---
+++ src/test/java/pphvaz/lojaspring/LojaVirtualSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pphvaz.lojaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
