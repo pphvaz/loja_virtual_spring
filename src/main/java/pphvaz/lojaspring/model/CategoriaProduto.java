6cb5ccfa9cb2e73b431684a3af981bcfec723faf
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-22 11:01:20 -0300

    Criado Entidade CategoriaProduto

--- src/main/java/pphvaz/lojaspring/model/CategoriaProduto.java
+++ src/main/java/pphvaz/lojaspring/model/CategoriaProduto.java
@@ -12,18 +12,17 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name="categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName="seq_categoria_produto", allocationSize=1, initialValue=1)
+public class CategoriaProduto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
-	private Long id;
-
 	
-	@Column(nullable = false)
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
+	private Long id;
+	
+	@Column(name="nome_desc", nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
@@ -55,8 +54,10 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
+	
+	
 
 }
