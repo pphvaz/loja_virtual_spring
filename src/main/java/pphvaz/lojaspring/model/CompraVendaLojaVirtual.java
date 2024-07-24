78db673b252e78ffd8a7d892c443b46db1e55002
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-24 14:02:02 -0300

    Criação Entidades CompraVendaLojaVirtual, NotaFiscalVenda e StatRastreio

---
+++ src/main/java/pphvaz/lojaspring/model/CompraVendaLojaVirtual.java
@@ -0,0 +1,192 @@
+package pphvaz.lojaspring.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "vd_cp_lj_virt")
+@SequenceGenerator(name = "seq_vd_cp_lj_virt", sequenceName = "seq_vd_cp_lj_virt", allocationSize = 1, initialValue = 1)
+public class CompraVendaLojaVirtual implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_vd_cp_lj_virt")
+	private Long id;
+
+	private BigDecimal valorTotal;
+
+	private BigDecimal valorDesconto;
+
+	private BigDecimal valorFrete;
+
+	private Integer diasEntrega;
+
+	@Temporal(TemporalType.DATE)
+	private Date dtVenda;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataEntrega;
+
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private Pessoa comprador;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_entrega_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "endereco_entrega_fk"))
+	private Endereco enderecoEntrega;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_cobranca_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "endereco_cobranca_fk"))
+	private Endereco enderecoCobranca;
+
+	@ManyToOne
+	@JoinColumn(name = "forma_pagamento_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "forma_pagamento_fk"))
+	private FormaDePagamento formaDePagamento;
+
+	@OneToOne
+	@JoinColumn(name = "nota_fiscal_venda_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "nota_fiscal_venda_fk"))
+	private NotaFiscalVenda notaFiscalVenda;
+
+	@ManyToOne
+	@JoinColumn(name = "cupom_de_desconto_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "cupom_de_desconto_fk"))
+	private CupomDeDesconto cupomDeDesconto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getValorDesconto() {
+		return valorDesconto;
+	}
+
+	public void setValorDesconto(BigDecimal valorDesconto) {
+		this.valorDesconto = valorDesconto;
+	}
+
+	public BigDecimal getValorFrete() {
+		return valorFrete;
+	}
+
+	public void setValorFrete(BigDecimal valorFrete) {
+		this.valorFrete = valorFrete;
+	}
+
+	public Integer getDiasEntrega() {
+		return diasEntrega;
+	}
+
+	public void setDiasEntrega(Integer diasEntrega) {
+		this.diasEntrega = diasEntrega;
+	}
+
+	public Date getDtVenda() {
+		return dtVenda;
+	}
+
+	public void setDtVenda(Date dtVenda) {
+		this.dtVenda = dtVenda;
+	}
+
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+
+	public Pessoa getComprador() {
+		return comprador;
+	}
+
+	public void setComprador(Pessoa comprador) {
+		this.comprador = comprador;
+	}
+
+	public Endereco getEnderecoEntrega() {
+		return enderecoEntrega;
+	}
+
+	public void setEnderecoEntrega(Endereco enderecoEntrega) {
+		this.enderecoEntrega = enderecoEntrega;
+	}
+
+	public Endereco getEnderecoCobranca() {
+		return enderecoCobranca;
+	}
+
+	public void setEnderecoCobranca(Endereco enderecoCobranca) {
+		this.enderecoCobranca = enderecoCobranca;
+	}
+
+	public FormaDePagamento getFormaDePagamento() {
+		return formaDePagamento;
+	}
+
+	public void setFormaDePagamento(FormaDePagamento formaDePagamento) {
+		this.formaDePagamento = formaDePagamento;
+	}
+
+	public NotaFiscalVenda getNotaFiscalVenda() {
+		return notaFiscalVenda;
+	}
+
+	public void setNotaFiscalVenda(NotaFiscalVenda notaFiscalVenda) {
+		this.notaFiscalVenda = notaFiscalVenda;
+	}
+
+	public CupomDeDesconto getCupomDeDesconto() {
+		return cupomDeDesconto;
+	}
+
+	public void setCupomDeDesconto(CupomDeDesconto cupomDeDesconto) {
+		this.cupomDeDesconto = cupomDeDesconto;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CompraVendaLojaVirtual other = (CompraVendaLojaVirtual) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
