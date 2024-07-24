030f8b3ea7ca650a3cb44eb952fa40a7b4a13229
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-24 10:18:38 -0300

    Criado Entidade ContaReceber, pacote cb.migration e SQL para ser usado em produção do DB.

--- src/main/java/pphvaz/lojaspring/model/Usuario.java
+++ src/main/java/pphvaz/lojaspring/model/Usuario.java
@@ -49,7 +49,7 @@ public class Usuario implements UserDetails {
 			name = "unique_acesso_user"),
 			joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
 			inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id", table = "acesso", foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT))
-			)
+		)
 	
 	private List<Acesso> acessos;
 	
