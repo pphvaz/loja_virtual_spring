e086b0c86dc5bbf16a549a3886471d96ec828584
Pedro Alves <pedrohvs.alves@gmail.com>
2024-07-25 14:38:01 -0300

    Criando anotações no SpringAppRun

--- src/main/java/pphvaz/lojaspring/LojaVirtualSpringApplication.java
+++ src/main/java/pphvaz/lojaspring/LojaVirtualSpringApplication.java
@@ -2,8 +2,16 @@ package pphvaz.lojaspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EntityScan(basePackages = "pphvaz.lojaspring.model")
+@ComponentScan(basePackages = {"pphvaz.*"})
+@EnableJpaRepositories(basePackages = {"pphvaz.lojaspring.repository"})
+@EnableTransactionManagement
 public class LojaVirtualSpringApplication {
 
 	public static void main(String[] args) {
